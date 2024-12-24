4453347bd3683d5b0d9ef93a96faef7dc4c13374
evgLip <evggenlip@gmail.com>
2024-12-24 21:19:41 +0300

    в CreateCabinForm повторяющийся код вынесен в FormRow

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,6 +1,5 @@
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import styled from "styled-components";
 import toast from "react-hot-toast";
 
 import Input from "../../ui/Input";
@@ -9,42 +8,8 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { createCabin } from "../../services/apiCabins";
+import FormRow from "../../ui/FormRow";
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
 
 function CreateCabinForm ()
 {
@@ -65,21 +30,15 @@ function CreateCabinForm ()
     }
   );
 
-  function onSubmit (data)
-  {
-    mutate(data);
-  }
+  function onSubmit (data) { mutate(data); }
 
-  function onError (errors)
-  {
-    console.log(errors);
-  }
+  function onError (errors) { console.log(errors); }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit, onError)}>
-      <FormRow>
-        <Label htmlFor="name">Наименование коттеджа</Label>
-        <Input type="text" id="name"
+
+      <FormRow label='Наименование коттеджа' error={errors?.name?.message}>
+        <Input type="text" id="name" disabled={isCreating}
           {
           ...register('name',
             {
@@ -88,12 +47,10 @@ function CreateCabinForm ()
 
           )}
         />
-        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Вместимость</Label>
-        <Input type="number" id="maxCapacity"
+      <FormRow label='Вместимость' error={errors?.maxCapacity?.message}>
+        <Input type="number" id="maxCapacity" disabled={isCreating}
           {
           ...register('maxCapacity',
             {
@@ -106,12 +63,10 @@ function CreateCabinForm ()
 
           )}
         />
-        {errors?.maxCapacity?.message && <Error>{errors.maxCapacity.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Цена</Label>
-        <Input type="number" id="regularPrice"
+      <FormRow label='Цена' error={errors?.regularPrice?.message}>
+        <Input type="number" id="regularPrice" disabled={isCreating}
           {
           ...register('regularPrice',
             {
@@ -124,12 +79,10 @@ function CreateCabinForm ()
 
           )}
         />
-        {errors?.regularPrice?.message && <Error>{errors.regularPrice.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="discount">Скидка</Label>
-        <Input type="number" id="discount" defaultValue={0}
+      <FormRow label='Скидка' error={errors?.discount?.message}>
+        <Input type="number" id="discount" defaultValue={0} disabled={isCreating}
           {
           ...register('discount',
             {
@@ -139,12 +92,10 @@ function CreateCabinForm ()
 
           )}
         />
-        {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="description">Описание</Label>
-        <Textarea type="number" id="description" defaultValue=""
+      <FormRow label='Описание' error={errors?.description?.message}>
+        <Textarea type="number" id="description" defaultValue="" disabled={isCreating}
           {
           ...register('description',
             {
@@ -153,11 +104,9 @@ function CreateCabinForm ()
 
           )}
         />
-        {errors?.description?.message && <Error>{errors.description.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
+      <FormRow label='Фото коттеджа'>
         <FileInput id="image" accept="image/*" />
       </FormRow>
 
