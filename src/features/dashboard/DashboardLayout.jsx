1f0c53874e6cfb008e23bfc7cba51eadef2e6912
evgLip <evggenlip@gmail.com>
2025-01-21 18:55:00 +0300

    макет dashboard Dashboard.jsx DashboardLayout.jsx DashboardFilter.jsx

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout ()
+{
+
+  return (
+    <StyledDashboardLayout>
+      <div>Статистика</div>
+      <div>Текущая операции</div>
+      <div>График продолжительности пребывания</div>
+      <div>График продаж</div>
+    </StyledDashboardLayout>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,18 @@
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Dashboard ()
 {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Панель инструментов</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Панель инструментов</Heading>
+        <DashboardFilter />
+      </Row>
+      <DashboardLayout />
+    </>
   );
 }
 
